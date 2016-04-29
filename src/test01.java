51e704e647e26816f1ac86f51932dc6f00e084fb
Administrator <Administrator@PC-20160222TWPT>
2016-04-29 09:01:20 +0800

    test

---
+++ src/test01.java
@@ -0,0 +1,13 @@
+
+public class test01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hellow world");
+
+	}
+
+}
